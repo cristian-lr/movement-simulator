c20dfe891bbc6cd724e1c4000b8966c5b4a0e43e
cristian-lr <126866269+cristian-lr@users.noreply.github.com>
2023-04-19 19:29:22 +0100

    (feat): Comienzo clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import net.salesianos.lacuesta.menu.MenuDeJuego;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
+        System.out.print("Escribe el tamaño del mapa: ");
+        int tamanioMapa = scanner.nextInt();
+        scanner.nextLine();
+        MenuDeJuego menuDeJuego = new MenuDeJuego(tamanioMapa, "usuario");
+
     }
 }
